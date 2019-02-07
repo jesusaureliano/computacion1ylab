62afccf1856730f8b40e0d7abb2aa2ce58c96641
jesusaureliano <38467987+jesusaureliano@users.noreply.github.com>
2019-02-07 10:35:43 -0600

    Add files via upload

---
+++ mains3p5.cpp
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   proyecto1s3.cpp
+ * Author: jesusesquivel
+ *
+ * Created on 7 de febrero de 2019, 10:14 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    char c;
+    for(int i = 0; i <=255; i++){
+        c = i;
+        std::cout << i << " : " << c << endl;
+    }
+    
+    return 0;
+}
+
